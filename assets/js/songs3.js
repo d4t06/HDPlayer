e59356f00dcb7a3471f4acb43aed4b290f38c246
d4t06 <111503885+d4t06@users.noreply.github.com>
2023-01-17 19:33:30 +0700

    update image path songs3

--- assets/js/songs3.js
+++ assets/js/songs3.js
@@ -15,7 +15,7 @@ const songs3 = [
     name: "We Own The Night",
     singer: "The Wanted",
     path: "https://data56.chiasenhac.com/downloads/1167/1/1166056-ce007c66/128/We%20Own%20The%20Night%20-%20The%20Wanted.mp3",
-    image: "https://data.chiasenhac.com/data/cover/100/99667.jpg",
+    image: "https://data.chiasenhac.com/data/cover/12/11830.jpg",
   },
   {
     name: "A Sky Full Of Star",
@@ -27,20 +27,20 @@ const songs3 = [
     name: "Sugar",
     singer: "Maroon 5",
     path: "https://data62.chiasenhac.com/downloads/1349/1/1348625-00a323af/128/Sugar%20-%20Maroon%205.mp3",
-    image: "/assets/images/20220911_121743.jpg",
+    image: "https://data.chiasenhac.com/data/cover/29/28216.jpg",
   },
   {
     name: "You And I",
     singer: "Huu Dat & Ngoc Mai",
     path:
       "https://data56.chiasenhac.com/downloads/1176/1/1175158-9e3c33b1/128/You%20And%20I%20-%20One%20Direction.mp3",
-    image: "/assets/images/928D197B-717F-4A89-AB80-1819026B4168.jpeg"
+    image: "https://data.chiasenhac.com/data/cover/13/12809.jpg"
   },
   {
     name: "Perfect",
     singer: "Ed Sheeran",
     path: "https://data17.chiasenhac.com/downloads/2153/1/2152901-c30ec5b5/128/Perfect%20-%20Ed%20Sheeran.mp3",
-    image: "/assets/images/IMG_20220913_230034.jpg",
+    image: "https://data.chiasenhac.com/data/cover/120/119595.jpg",
   },
   {
     name: "Take Me To Infinity",
