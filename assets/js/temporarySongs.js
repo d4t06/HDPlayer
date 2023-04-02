e59197b557ffcd3f90742f8626f100290ed9f9b5
d4t06 <huudat01234560@gmail.com>
2023-04-02 20:50:28 +0700

    add song

--- assets/js/temporarySongs.js
+++ assets/js/temporarySongs.js
@@ -24,28 +24,12 @@ export const temporarySongs = [
         image: "https://e-cdns-images.dzcdn.net/images/cover/d1458579e53353f4278f2e77ffc75b3b/500x500-000000-80-0-0.jpg"
       },
       {
-        name: "Sugar",
-        singer: "Maroon 5",
+        name: "Stressed Out",
+        singer: "Twenty One Pilot",
         path:
-          "/assets/songs/Maroon 5 - Sugar.mp3",
+          "/assets/songs/Twenty One Pilots - Stressed Out.mp3",
     
-        image: "https://e-cdns-images.dzcdn.net/images/cover/f0a1d4442389bcd7919a7054f8c0b785/500x500-000000-80-0-0.jpg"
-      },
-      {
-        name: "Perfect",
-        singer: "Ed Sheeran",
-        path:
-          "/assets/songs/Ed Sheeran - Perfect.mp3",
-    
-        image: "https://e-cdns-images.dzcdn.net/images/cover/000a9228cecfcc7c2093d9cd7bb66447/500x500-000000-80-0-0.jpg"
-      },
-      {
-        name: "Steal My Girls",
-        singer: "One Direction",
-        path:
-          "/assets/songs/One Direction - Steal My Girl.mp3",
-    
-        image: "https://e-cdns-images.dzcdn.net/images/cover/d1458579e53353f4278f2e77ffc75b3b/500x500-000000-80-0-0.jpg"
+        image: "https://e-cdns-images.dzcdn.net/images/cover/48e4d114036a2b4ac24f7fadb44a3c4d/500x500-000000-80-0-0.jpg"
       },
       {
         name: "Sugar",
@@ -71,5 +55,44 @@ export const temporarySongs = [
     
         image: "https://e-cdns-images.dzcdn.net/images/cover/d1458579e53353f4278f2e77ffc75b3b/500x500-000000-80-0-0.jpg"
       },
+      {
+        name: "Stressed Out",
+        singer: "Twenty One Pilot",
+        path:
+          "/assets/songs/Twenty One Pilots - Stressed Out.mp3",
+    
+        image: "https://e-cdns-images.dzcdn.net/images/cover/48e4d114036a2b4ac24f7fadb44a3c4d/500x500-000000-80-0-0.jpg"
+      },
+      {
+        name: "Sugar",
+        singer: "Maroon 5",
+        path:
+          "/assets/songs/Maroon 5 - Sugar.mp3",
+    
+        image: "https://e-cdns-images.dzcdn.net/images/cover/f0a1d4442389bcd7919a7054f8c0b785/500x500-000000-80-0-0.jpg"
+      },
+      {
+        name: "Perfect",
+        singer: "Ed Sheeran",
+        path:
+          "/assets/songs/Ed Sheeran - Perfect.mp3",
+    
+        image: "https://e-cdns-images.dzcdn.net/images/cover/000a9228cecfcc7c2093d9cd7bb66447/500x500-000000-80-0-0.jpg"
+      },
+      {
+        name: "Steal My Girls",
+        singer: "One Direction",
+        path:
+          "/assets/songs/One Direction - Steal My Girl.mp3",
+    
+        image: "https://e-cdns-images.dzcdn.net/images/cover/d1458579e53353f4278f2e77ffc75b3b/500x500-000000-80-0-0.jpg"
+      },{
+        name: "Stressed Out",
+        singer: "Twenty One Pilot",
+        path:
+          "/assets/songs/Twenty One Pilots - Stressed Out.mp3",
+    
+        image: "https://e-cdns-images.dzcdn.net/images/cover/48e4d114036a2b4ac24f7fadb44a3c4d/500x500-000000-80-0-0.jpg"
+      }
 
 ]
